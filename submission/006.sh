f1f89f8584d55f7b0e71db8834b8930bb7a54a70
Deepak Vashist <deepaksvashist@gmail.com>
2024-11-26 11:59:15 -0300

    submit scripts

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,15 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+
+BLOCK_COINBASE=256128
+BLOCK_SPENDING=257343
+
+COINBASE_TXNID=$(bitcoin-cli getblock $(bitcoin-cli getblockhash $BLOCK_COINBASE) | jq -r '.tx[0]')
+TXN_IDS=$(bitcoin-cli getblock $(bitcoin-cli getblockhash $BLOCK_SPENDING) | jq -r '.tx[]')
+
+for TXN_ID in $TXN_IDS; do
+  RAW_TX=$(bitcoin-cli getrawtransaction "$TXN_ID" true)
+  if echo "$RAW_TX" | jq -e --arg coinbase_txnid "$COINBASE_TXNID" '.vin[] | select(.txid == $coinbase_txnid)' > /dev/null; then
+    echo $TXN_ID
+    exit 0
+  fi
+done
--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,9 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+
+TXN_ID="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+TXN_INFO=$(bitcoin-cli getrawtransaction $TXN_ID true)
+KEY=$(echo $TXN_INFO | jq -r '.vin[0].txinwitness[2]')
+PUBLIC_KEY=${KEY:4:66}
+
+echo $PUBLIC_KEY
